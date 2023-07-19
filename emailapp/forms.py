c832a466af90d9269cb1ff8418310058f14df216
Nandhini-pinesphere11 <nandhini@pinesphere.com>
2023-07-19 12:44:39 +0530

    second commit

--- emailapp/forms.py
+++ emailapp/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class EmailForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
-    recipient = forms.EmailField(label='To')
+    recipients = forms.CharField(label='To', widget=forms.Textarea)
     message = forms.CharField(widget=forms.Textarea)
--- emailapp/views.py
+++ emailapp/views.py
@@ -1,29 +1,29 @@
-from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from .forms import EmailForm
-
+from django.core.mail import send_mail
 
 def send_email(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
-            recipient = form.cleaned_data['recipient']
+            recipients = [email.strip() for email in form.cleaned_data['recipients'].split(',')]
             message = form.cleaned_data['message']
-            
+
             send_mail(
                 'Subject',
                 message,
-                name + ' <sender@example.com>',  # Replace 'sender@example.com' with your actual email address
-                [recipient],
+                'sender@example.com',
+                recipients,
                 fail_silently=False,
             )
-            
+
             return redirect('email_success')
     else:
         form = EmailForm()
-    
+
     return render(request, 'email_form.html', {'form': form})
+
 
 def email_success(request):
     return render(request, 'email_success.html')
